80048f6af4f9c1598116075e2c77ebccac67357f
Mara Green <maragreen42@gmail.com>
2023-01-02 20:43:22 -0800

    added GET api call, test in Postman completes

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Song
+from . serializer import SongSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+@api_view(['GET'])
+def song_list(request):
+    if request.method == 'GET':
+        song_name = request.query_params.get('song_id')
+        songs = Song.objects.all()
+
+        if song_name:
+            songs = songs.filter(song_id=song_name)
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
