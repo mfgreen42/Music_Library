40f4a59c60858184f46f75e29ceb7a9db849c4e0
Mara Green <maragreen42@gmail.com>
2023-01-02 19:48:21 -0800

    create Songs app

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -1,4 +1,4 @@
-"""music_libray_project URL Configuration
+"""music_library_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
